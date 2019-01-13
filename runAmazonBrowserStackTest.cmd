e5ad9005381db0e4bb6df4b7f7d5491e84497850
Joe <j.mcmanamon@hotmail.com>
2019-01-13 22:36:43 +1300

    Selenium working with browserstack, currenly only Windows devices

--- runAmazonBrowserStackTest.cmd
+++ runAmazonBrowserStackTest.cmd
@@ -1,1 +1,2 @@
-call ./gradlew clean :cleanTest :test --tests com.amazon.testAmazon.amazonSearchForBookViaBrowserStack -Dmode="local" -Dbrowser="Chrome"
+call ./gradlew clean :cleanTest :test --tests com.amazon.testAmazon.amazonSearchForBookViaBrowserStack -Dos="Windows" -Dos_version="7" -Dbrowser="Chrome" -Dbrowser_version="70.0"
+call ./gradlew clean :cleanTest :test --tests com.amazon.testAmazon.amazonSearchForBookViaBrowserStack -Dos="Windows" -Dos_version="10" -Dbrowser="Chrome" -Dbrowser_version="70.0"
--- src/test/java/com/amazon/testAmazon.java
+++ src/test/java/com/amazon/testAmazon.java
@@ -41,13 +41,20 @@ public class testAmazon extends baseTestTheHomePage {
 
         WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
         searchBox.sendKeys("hitchhikers");
+        System.out.println("Typed in search box");
+
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         searchBox.sendKeys(Keys.ARROW_DOWN);
         searchBox.sendKeys(Keys.RETURN);
+        System.out.println("Down arrow and enter keys pressed");
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         driver.findElement(By.linkText("The Hitchhiker's Guide to the Galaxy")).click();
+        System.out.println("Clicked on book link");
+
         driver.manage().timeouts().implicitlyWait(2, TimeUnit.SECONDS);
         driver.findElement(By.id("add-to-cart-button")).click();
+        System.out.println("Added book to cart");
+
 
         sleep(1000);
 //        driver.quit();
@@ -60,7 +67,6 @@ public class testAmazon extends baseTestTheHomePage {
 
     @Test
     @Category(RegressionTests.class)
-    @FileParameters("src/test/resources/NewEmployeeTestParameters.csv")
     public void amazonSearchForBookViaBrowserStack() throws MalformedURLException, InterruptedException {
 //        GetDesiredCapabilities();
         DesiredCapabilities caps = new DesiredCapabilities();
@@ -74,9 +80,6 @@ public class testAmazon extends baseTestTheHomePage {
             caps.setCapability("resolution", resolution);
         }
 
-
-
-
         if (browserTesting.equals("false")) {
 
             caps.setCapability("browserName", browserName);
@@ -86,54 +89,17 @@ public class testAmazon extends baseTestTheHomePage {
             caps.setCapability("browser", browser);
             caps.setCapability("browser_version", browser_version);
         }
-//        For Browser running on a Windows machine...here's an example of the required capabilities
-//        DesiredCapabilities caps = new DesiredCapabilities();
-//        caps.setCapability("browser", "Chrome");
-//        caps.setCapability("browser_version", "62.0");
-//        caps.setCapability("os", "Windows");
-//        caps.setCapability("os_version", "10");
-//        caps.setCapability("resolution", "1024x768");
-
-//        For Android/IOS device...here's an example of the required capabilities
-//        DesiredCapabilities caps = new DesiredCapabilities();
-//        caps.setCapability("browserName", "android");
-//        caps.setCapability("device", "Samsung Galaxy S8 Plus");
-//        caps.setCapability("realMobile", "true");
-//        caps.setCapability("os_version", "7.0");
 
         java.net.URL browserStackURL = new URL(BSURL);
         WebDriver driver = new RemoteWebDriver(browserStackURL, caps);
         driver.manage().window().maximize();
-//        WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
-//        driver.get("http://www.google.com");
 
         driver.get(URL);
-        sleep(2000);
-        driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
-//        driver.findElement(By.id("__input1-inner")).sendKeys("crichardson");
-        driver.findElement(By.id("__input1-inner")).sendKeys(username001);
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-//        driver.findElement(By.id("__input2-inner")).sendKeys("Zag2018!");
-        driver.findElement(By.id("__input2-inner")).sendKeys(password001);
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-        driver.findElement(By.id("__button2-content")).click();
-        sleep(7000);
-        // Type "Add New Employee" in the search field
-        driver.findElement(By.id("bizXSearchField-I")).sendKeys(valueToBeSearched);
-        System.out.println("***typed the search item");
-        sleep(5000);
+        WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
+        searchBox.sendKeys("hitchhikers");
+        System.out.println("Typed in search box");
 
-//        Send the Enter Key twice
-        WebElement textbox = driver.findElement(By.id("bizXSearchField-I"));
-        sleep(3000);
-        textbox.sendKeys(Keys.RETURN);
-        sleep(3000);
-        textbox.sendKeys(Keys.RETURN);
-        // Click on the search button
-//        driver.findElement(By.xpath("//input[@value='Go']")).click();
-//        driver.findElement(By.xpath("//*[@id='submit-search']")).click();
-        System.out.println("***clicked on the search button");
-        sleep(10000);
+        driver.quit();
 
 
     }
