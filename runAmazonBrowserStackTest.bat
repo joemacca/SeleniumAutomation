f14a3ab98bab80f1b7f6dd37054addd28512aec8
Joe <j.mcmanamon@hotmail.com>
2019-01-14 19:49:34 +1300

    Firefox tests

--- runAmazonBrowserStackTest.bat
+++ runAmazonBrowserStackTest.bat
@@ -1,4 +1,2 @@
-call ./gradlew clean :cleanTest :test --tests com.amazon.testAmazon.amazonSearchForBookViaBrowserStack -Dos="Windows" -Dos_version="7" -Dbrowser="Chrome" -Dbrowser_version="70.0"
-call ./gradlew clean :cleanTest :test --tests com.amazon.testAmazon.amazonSearchForBookViaBrowserStack -Dos="Windows" -Dos_version="10" -Dbrowser="Chrome" -Dbrowser_version="70.0"
-call ./gradlew clean :cleanTest :test --tests com.amazon.testAmazon.amazonSearchForBookViaBrowserStack -Dos="Windows" -Dos_version="7" -Dbrowser="Firefox" -Dbrowser_version="70.0"
-call ./gradlew clean :cleanTest :test --tests com.amazon.testAmazon.amazonSearchForBookViaBrowserStack -Dos="Windows" -Dos_version="10" -Dbrowser="Firefox" -Dbrowser_version="70.0"
+call ./gradlew clean :cleanTest :test --tests com.amazon.testAmazon.amazonSearchForBookViaBrowserStack -Dos="Windows" -Dos_version="7" -Dbrowser="Firefox"
+call ./gradlew clean :cleanTest :test --tests com.amazon.testAmazon.amazonSearchForBookViaBrowserStack -Dos="Windows" -Dos_version="10" -Dbrowser="Firefox"
